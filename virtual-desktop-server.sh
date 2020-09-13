fc8c73185a6a97e0099b3358a5b3a5d6b58aa214
Eric <eric@kleincore.co.za>
2020-09-13 20:47:02 +0200

    Completed the file
    Going for testing

--- virtual-desktop-server.sh
+++ virtual-desktop-server.sh
@@ -16,10 +16,48 @@ echo "Your UID is ${UID}."
 if [[ "${UID}" -eq 0 ]] # The spaces in the if statment are very important!
 then
 	echo "Now installing software"
+	read -n 1 -s -r -p "Press any key to continue"
+	echo " "
+	echo "Updating you system"
+	apt-get update && apt upgrade -y
+	read -n 1 -s -r -p "Press any key to continue"
+	echo " "
+	echo "Installing Remote desktop system!"
+	apt-get install xrdp -y
+	read -n 1 -s -r -p "Press any key to continue"
+	echo " "
+	
+	echo "Please select a desktop environment (GUI):"
+while :
+do
+  read GUI
+  case $GUI in
+	MATE)
+		echo "You have chosen MATE Desktop"
+		apt-get install -y mate-core mate-desktop-environment mate-notification-daemon
+		sed -i.bak '/fi/a #xrdp multiple users configuration \n mate-session \n' /etc/xrdp/startwm.sh
+		break
+		;;
+	XFCE)
+		echo "You have chosen XFCE4 Desktop"
+		apt-get install -y xfce4 xfce4-terminal
+		break
+		;;
+	*)
+		echo "Invalid option"
+		;;
+  esac
+done
+echo " "
+
+adduser xrdp ssl-cert
+ufw allow 3389/tcp
+/etc/init.d/xrdp restart
+
 
 
 else
-	echo "You re not root and therefor may not install software. Please use root or sudo command."
+	echo "You re not root and therefore may not install software. Please use root or sudo command."
 fi
 
 #
