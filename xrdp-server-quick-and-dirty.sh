aaee35e35a1cfa1e9c49b6333a0b3828630164a3
darkjedeye <eric@kleincore.co.za>
2020-10-21 08:01:37 +0200

    added auto -y

--- xrdp-server-quick-and-dirty.sh
+++ xrdp-server-quick-and-dirty.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
-sudo apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils
-sudo apt install xrdp 
+sudo apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils -y
+sudo apt install xrdp -y
 sudo adduser xrdp ssl-cert  
 sudo systemctl restart xrdp
 sudo ufw allow 3389
