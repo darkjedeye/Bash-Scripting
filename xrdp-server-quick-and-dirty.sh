21b33b4846b1b958d5e9a804e2b7146a178a3512
darkjedeye <eric@kleincore.co.za>
2020-10-21 07:57:15 +0200

    newscript

---
+++ xrdp-server-quick-and-dirty.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+sudo apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils
+sudo apt install xrdp 
+sudo adduser xrdp ssl-cert  
+sudo systemctl restart xrdp
+sudo ufw allow 3389
+sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
