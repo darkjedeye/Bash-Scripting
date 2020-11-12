bc05bf0b69ff0765e5d6a10827adca584b54aa0b
darkjedeye <eric@kleincore.co.za>
2020-11-12 10:32:08 +0200

    tidied the Cockpit-Install.sh file a bit

--- Cockpit-install.sh
+++ Cockpit-install.sh
@@ -5,16 +5,22 @@ echo "Your UID is ${UID}."
 #check root
 if [[ "${UID}" -eq 0 ]]
 then
+echo "Updateint the system!"
+read -t 10 -n 1 -s -r -p "Press any key to continue"
+apt update
+apt upgrade -y && apt dist-upgrade -y
 echo "This script will install Cockpit on your system. Are you sure you would like to do this?: (y/n)" 
 read ANSWER
 if [[ $ANSWER = "y" ]]
 then
-apt-get install cockpit
+apt-get install cockpit -y
 systemctl enable --now cockpit.socket
 netstat -a | grep 9090
 systemctl enable --now cockpit.socket
 ufw allow 9090
 ufw allo ssh
+echo "now removing unneccesary files!"
+read -t 10 -n 1 -s -r -p "Press any key to continue"
 else 
 echo "why run the script?"
 fi
