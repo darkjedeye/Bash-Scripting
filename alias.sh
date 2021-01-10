0f334c7a1e30b7c231cd25fec9fb148c8ebf91fe
darkjedeye <eric@kleincore.co.za>
2021-01-10 21:38:08 +0200

    added aliases

--- alias.sh
+++ alias.sh
@@ -44,6 +44,9 @@ echo " "
  echo 'alias      upgrade="sudo apt upgrade -y"'  >> /etc/bash.bashrc
  echo 'alias      cls="clear"'  >> /etc/bash.bashrc
  echo 'alias      matrix="cmatrix"'  >> /etc/bash.bashrc
+ echo 'alias	  external="dig +short myip.opendns.com @resolver1.opendns.com"' >> /etc/bash.bashrc
+ echo 'alias      antserv="ssh steam@kleincore.us.to"' >> /etc/bash.bashrc
+ echo 'alias	  remgit="git config --global credential.helper store"' >> /etc/bash.bashrc
  echo ' ' >> /etc/bash.bashrc
  echo ' neofetch' >> /etc/bash.bashrc
  echo '/usr/games/fortune | /usr/games/cowthink -f tux' >> /etc/bash.bashrc
