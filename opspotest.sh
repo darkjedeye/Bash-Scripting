5aeca098d0394fed6e2474018cf4b66615a77e9a
darkjedeye <eric@kleincore.co.za>
2021-01-13 09:40:35 +0200

    cleaned up

--- opspotest.sh
+++ opspotest.sh
@@ -7,7 +7,8 @@ if [[ "${UID}" -eq 0 ]]
 then
 echo "Updating system."
 read -t 5 -n 1 -s -r -p "Press any key to continue"
-echo
+echo " "
+add-apt-repository ppa:ondrej/php
 apt-get update && apt-get dist-upgrade -y 
 echo "please wait while we install the rest"
 read -t 5 -n 1 -s -r -p "Press any key to continue"
@@ -41,7 +42,7 @@ echo " "
 echo "Now installing PHP!"
   read -t 5 -n 1 -s -r -p "Press any key to continue"
 echo " "
-apt-get install php libapache2-mod-php php-mcrypt php-mysql php-intl php-gd php-bcmath php-common php-curl phpmyadmin -y
+apt-get install php7.4 libapache2-mod-php php-mcrypt php-mysql php-intl php-gd php-bcmath php-common php-curl phpmyadmin -y
 echo "Done!"
   read -t 5 -n 1 -s -r -p "Press any key to continue"
 echo " "
@@ -57,6 +58,9 @@ cat /etc/apache2/ports.conf
 read -t 5 -n 1 -s -r -p "Press any key to continue"
 echo "Please give desired port ranging from 80 - 89"
 read Port
+last=$((Port-1))
+listing="Listen $Port"
+sed -i 's|Listen '"$last"'|'"$listing"'|' /etc/apache2/ports.conf
 cd /var/www/
 mkdir $DIR
 cd /var/www/$DIR
@@ -64,10 +68,14 @@ wget https://github.com/opensourcepos/opensourcepos/releases/download/3.3.2/open
 apt install unzip -y
 unzip opensourcepos.20200903075833.3.3.2.bb309c.zip
 rm opensourcepos.20200903075833.3.3.2.bb309c.zip
-cd /etc/apache2/sites-available
-cp 000-default.conf 000-default.old
-sed -i 's|DocumentRoot /var/www/html|DocumentRoot /var/www/'"$DIR"'|' /etc/apache2/sites-available/000-default.conf
-sed -i 's|^<VirtualHost[ ]\*:80>|^<VirtualHost[ ]\*:'"$Port"'>|' /etc/apache2/sites-available/000-default.conf 
+echo " please type in the name for your sites config file"
+read config
+site=$config.conf
+cd /etc/apache2/sites-available 
+cp 000-default.conf $site
+sed -i 's|DocumentRoot /var/www/html|DocumentRoot /var/www/'"$DIR"'/public|' /etc/apache2/sites-available/$site
+sed -i 's|<VirtualHost\*:80>|<VirtualHost\*:'"$Port"'>|' /etc/apache2/sites-available/$sites
+a2ensite $site
 echo "The next password prompt will be the same as the one you entered when installing Mysql in order to create the database"
   read -t 5 -n 1 -s -r -p "Press any key to continue"
 echo " "
@@ -77,7 +85,7 @@ cd /var/www/$DIR/database
 mysql -u root -p ospos < database.sql
 cd ../application/config
 key=$(openssl rand -base64 32) 
-sed -i 's|.|'"'""$key"'|83' config.php
+sed -i '361s|.|'"'""$key"'|83' config.php
 cd /etc/apache2
 cp apache2.conf apache2.old
 echo " "
