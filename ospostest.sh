69394d1183fe5f43e4fa5a81759084dce55727de
darkjedeye <eric@kleincore.co.za>
2021-01-14 08:17:50 +0200

    added global Variables

--- ospostest.sh
+++ ospostest.sh
@@ -2,6 +2,17 @@
 #check user status
 echo "Your UID is ${UID}."
 
+#Global Variables
+SERVER=" "
+DIR=" "
+Port=" "
+last=$((Port-1))
+listing="Listen $Port"
+config=" "
+site=$config.conf
+key=$(openssl rand -base64 32) 
+localaddress="ifconfig"
+
 #check root
 if [[ "${UID}" -eq 0 ]]
 then
@@ -14,52 +25,47 @@ echo "please wait while we install the rest"
 read -t 5 -n 1 -s -r -p "Press any key to continue"
 echo " "
 echo "Installing Apache Server"
-  read -t 5 -n 1 -s -r -p "Press any key to continue"
+read -t 5 -n 1 -s -r -p "Press any key to continue"
 echo " "
-echo "Please enter the url for your server." 
-read SERVER
+read -p "Please enter the url for your server." 
 apt-get install apache2 -y
 echo "Adding your server name to apache config."
 read -t 5 -n 1 -s -r -p "Press any key to continue"
 echo 'ServerName $SERVER' >> /etc/apache2/apache2.conf
-echo
+echo " "
 echo "Testing apache config."
 read -t 5 -n 1 -s -r -p "Press any key to continue"
 echo
 apache2ctl configtest
 ufw allow in "Apache Full"
 echo "Apache Server installed!"
-  read -t 5 -n 1 -s -r -p "Press any key to continue"
+read -t 5 -n 1 -s -r -p "Press any key to continue"
 echo " "
 echo "Now installing Mysql Server!"
-  read -t 5 -n 1 -s -r -p "Press any key to continue"
+read -t 5 -n 1 -s -r -p "Press any key to continue"
 echo " "
 apt install mysql-server -y
 echo "Mysql install done!"
 echo " "
-  read -t 5 -n 1 -s -r -p "Press any key to continue"
+read -t 5 -n 1 -s -r -p "Press any key to continue"
 echo " "
 echo "Now installing PHP!"
-  read -t 5 -n 1 -s -r -p "Press any key to continue"
+read -t 5 -n 1 -s -r -p "Press any key to continue"
 echo " "
 apt-get install php7.0 libapache2-mod-php php-mcrypt php-mysql php-intl php-gd php-bcmath php-common php-curl phpmyadmin -y
 echo "Done!"
-  read -t 5 -n 1 -s -r -p "Press any key to continue"
+read -t 5 -n 1 -s -r -p "Press any key to continue"
 echo " "
 a2enmod rewrite
 echo "Rewrite mod is enabed!"
 read -t 5 -n 1 -s -r -p "Press any key to continue"
 echo
 service apache2 restart
-echo "Please type the name for the directory you would like for install"
-read DIR
+read -p "Please type the name for the directory you would like for install"
 echo "These are the ports you have setup"
 cat /etc/apache2/ports.conf
 read -t 5 -n 1 -s -r -p "Press any key to continue"
-echo "Please give desired port ranging from 80 - 89"
-read Port
-last=$((Port-1))
-listing="Listen $Port"
+read -p "Please give desired port ranging from 80 - 89"
 sed -i 's|Listen '"$last"'|'"$listing"'|' /etc/apache2/ports.conf
 cd /var/www/
 mkdir $DIR
@@ -68,29 +74,26 @@ wget https://github.com/opensourcepos/opensourcepos/releases/download/3.3.2/open
 apt install unzip -y
 unzip opensourcepos.20200903075833.3.3.2.bb309c.zip
 rm opensourcepos.20200903075833.3.3.2.bb309c.zip
-echo " please type in the name for your sites config file"
-read config
-site=$config.conf
+read -p " please type in the name for your sites config file"
 cd /etc/apache2/sites-available 
 cp 000-default.conf $site
 sed -i 's|DocumentRoot /var/www/html|DocumentRoot /var/www/'"$DIR"'/public|' /etc/apache2/sites-available/$site
 sed -i 's|<VirtualHost\*:80>|<VirtualHost\*:'"$Port"'>|' /etc/apache2/sites-available/$sites
 a2ensite $site
 echo "The next password prompt will be the same as the one you entered when installing Mysql in order to create the database"
-  read -t 5 -n 1 -s -r -p "Press any key to continue"
+read -t 5 -n 1 -s -r -p "Press any key to continue"
 echo " "
 mysql -uroot -p -e "CREATE DATABASE ospos;CREATE USER 'admin'@'%' IDENTIFIED BY 'pointofsale';GRANT ALL PRIVILEGES ON ospos . * TO 'admin'@'%' IDENTIFIED BY 'pointofsale' WITH GRANT OPTION;FLUSH PRIVILEGES;"
 echo "The next password, Please type the same password you used for MYSQL in order to populate the database"
 cd /var/www/$DIR/database
 mysql -u root -p ospos < database.sql
 cd ../application/config
-key=$(openssl rand -base64 32) 
 sed -i '361s|.|'"'""$key"'|83' config.php
 cd /etc/apache2
 cp apache2.conf apache2.old
 echo " "
 sed -i '166s|AllowOverride None|AllowOverride All|' /etc/apache2/apache2.conf
-echo "Now you can visit your IP address in your browser. When you do, you'll see a login form.
+echo "Now you can visit your IP address '"$localaddress"' in your browser. When you do, you'll see a login form.
 Login Form
 Login using these credentials:
 Username: admin
